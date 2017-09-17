5a73873ae7a178060fc0483f8c5c6dd0926307fa
Jon Choi <choi.jonathanh@gmail.com>
2017-09-16 17:19:12 -0700

    closures

--- golang/tour_of_go/moretypes/more-types.go
+++ golang/tour_of_go/moretypes/more-types.go
@@ -3,7 +3,8 @@ package main
 import (
 	"fmt"
 	// "golang.org/x/tour/pic"
-	"golang.org/x/tour/wc"
+	// "golang.org/x/tour/wc"
+	"math"
 	"strings"
 )
 
@@ -323,6 +324,54 @@ func WordCount(s string) map[string]int {
 	return wc
 }
 
+func compute(fn func(float64, float64) float64) float64 {
+	return fn(3, 4)
+}
+
+func functions() {
+	hypot := func(x, y float64) float64 {
+		return math.Sqrt(x*x + y*y)
+	}
+	fmt.Println(hypot(5, 12))
+	fmt.Println(compute(hypot))
+	fmt.Println(compute(math.Pow))
+}
+
+func adder() func(int) int {
+	sum := 0
+	return func(x int) int {
+		sum += x
+		return sum
+	}
+}
+
+// A function value that refers to variables outside of its scope.
+func closures() {
+	// Each closure is bound to its own `sum` variable.
+	pos, neg := adder(), adder()
+	for i := 0; i < 10; i++ {
+		fmt.Println(
+			pos(i),
+			neg(-2*i),
+		)
+	}
+}
+
+func fibonacci() func() int {
+	a, b := 0, 1
+	return func() int {
+		a, b = b, a+b
+		return b - a
+	}
+}
+
+func printFibonacci() {
+	f := fibonacci()
+	for i := 0; i < 10; i++ {
+		fmt.Println(f())
+	}
+}
+
 func main() {
 	// pointers()
 	// structs()
@@ -342,5 +391,8 @@ func main() {
 	// maps()
 	// mapLiterals()
 	// mutateMaps()
-	wc.Test(WordCount)
+	// wc.Test(WordCount)
+	// functions()
+	// closures()
+	printFibonacci()
 }
